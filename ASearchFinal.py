4d638ea4f2864e65f6b29c5be6079297defdf0b9
Martha-Zhao <34574457+Martha-Zhao@users.noreply.github.com>
2019-03-15 23:10:31 +0800

    更新找到所有可能解

--- ASearchFinal.py
+++ ASearchFinal.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 #A_search.py
 #回溯法
-from cheseBoard.gVariables import Piece,CreateBoard,origin,end
-
+from findRoute.gVariables import Piece
 """
 A* find route
 father point in list myBoard.C
@@ -12,12 +11,7 @@ dead route in list myBoard.D
 distance : shortest route among son piece  
 search with backTracking
 """
-# def preEdit():
-#     myPiece = Piece(origin)# initialization my piece 
-#     myBoard = CreateBoard(origin,end) # initialization my board
-#     myBoard.placeBlock(1, 2, 3) 
-#     myBoard.placeBlock(0, 3, 3)  
-#     myBoard.placeBlock(1, 2, 2)
+
 temp = 1
 def searchPath(piece,board): 
     global temp
@@ -44,12 +38,82 @@ def searchPath(piece,board):
 #             myPiece = Piece(myBoard.S[-1])
 #             myBoard.C.append(myPiece.location)
 
+def possibleMoves(piece,testBoard):
+    moveList = []
+    x = piece.location[0]
+    y = piece.location[1]
+    """
+    up
+    """
+    if testBoard.board[x - 1,y * 3] == 1 and x > 0:  #piece upon
+        if testBoard.board[x - 2,y * 3 + 2] == -1 and x > 1: # block upon upon 
+            if testBoard.board[x - 1,y * 3 - 2] == 1 and y > 0:  #up left
+                moveList.append((x - 1,y - 1))
+            if testBoard.board[x - 1,y * 3 + 1] == 1: #up right
+                moveList.append((x - 1,y + 1))
+        elif x > 1:
+            moveList.append((x - 2,y)) #up up
+    elif testBoard.board[x - 1,y * 3 + 2] == 1 and x > 0: 
+        moveList.append((x - 1,y))# up
+        
+    """
+    down
+    """
+    if testBoard.board[x + 1, y * 3] == 1 and x < 6: #piece blow
+        if testBoard.board[x + 1, y * 3 + 2] == -1 : # block blow blow
+            if testBoard.board[x + 1,y * 3 - 2] == 1 and y > 0: #up left
+                moveList.append((x + 1,y - 1))
+            if testBoard.board[x + 1,y * 3 + 1] == 1:
+                moveList.append((x + 1,y + 1))  #up right
+        elif x < 5:
+            moveList.append((x + 2,y))        
+    elif testBoard.board[x, y * 3 + 2] == 1 and x < 6:
+        moveList.append((x + 1 ,y))
+    """
+    left
+    """
+    if testBoard.board[x,(y - 1) * 3] == 1 and y > 0: #piece left
+        if testBoard.board[x,(y - 2) * 3 + 1] == -1 and y > 1: #block left left
+            if testBoard.board[x - 1,(y - 1) * 3 + 2] == 1 and x > 0: # left upon
+                moveList.append((x - 1,y - 1))
+            if testBoard.board[x,(y - 1) * 3 + 2] == 1: # left blow
+                moveList.append((x + 1,y - 1))
+        elif y > 1:
+            moveList.append((x, y - 2)) #left left
+    elif testBoard.board[x, (y - 1)* 3 + 1] == 1 and y > 0:
+        moveList.append((x,y - 1)) #left
+        
+    """
+    right
+    """
+    if testBoard.board[x,(y + 1) * 3] == 1 and y < 6:  #piece right
+        if testBoard.board[x,(y + 1) * 3 + 1] == -1: #block right right
+            if testBoard.board[x - 1,(y + 1) * 3 + 2] == 1 and x > 0: #right upon
+                moveList.append((x - 1,y + 1))
+            if testBoard.board[x,(y + 1) * 3 + 2] == 1: # right blow
+                moveList.append((x + 1,y + 1))
+        elif y < 5:
+            moveList.append((x,y + 2)) #right right 
+    elif testBoard.board[x,y * 3 + 1] == 1 and y < 6:
+        moveList.append((x, y + 1)) # right
+    return moveList
 
-
-#main function
-# if __name__ == '__main__':
-
- 
-# result = searchPath()
-# print(result) #print result  
-            
+def possibleBlockLocation(piece,testBoard):
+    moveList = []
+    x = piece.location[0]
+    y = piece.location[1]
+    searchList = [(-2,0),(-1,0),(0,0),(1,0),
+                  (-2,-1),(-1,-1),(0,-1),(1,-1),
+                  (-2,-2),(-1,-2),(0,-2),(1,-2),
+                  (-2,1),(-1,1),(0,1),(1,1),
+                  (-2,2),(-1,2),(0,2),(1,2)]
+    for i in searchList:
+        if y + i[1] < 0 or y + i[1] > 5 or x + i[0] < 0 or x + i[0] > 5 or (0,x + i[0],y + i[1]) in testBoard.blocksList or (1,x + i[0],y + i[1]) in testBoard.blocksList:
+            pass
+        else:
+            if testBoard.board[x + i[0] ,(y + i[1]) * 3 + 1] == 1 and testBoard.board[x + i[0] + 1 ,(y + i[1]) * 3 + 1] == 1:
+                moveList.append((1,x + i[0],y + i[1]))
+            if testBoard.board[x + i[0] ,(y + i[1]) * 3 + 2] == 1 and testBoard.board[x + i[0],(y + i[1] + 1) * 3 + 2] == 1:
+                moveList.append((0,x + i[0],y + i[1]))
+    return moveList    
+    

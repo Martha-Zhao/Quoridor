ea708e0213544434b75f5a7eb1c8c910ddf6821f
Martha-Zhao <34574457+Martha-Zhao@users.noreply.github.com>
2019-03-21 23:19:51 +0800

    测试基本完成
    改进findNeighbor（）函数，该函数的调用顺序尚需完善
    改进evaluationFunc（），排除棋子自身原始位置对于路径规划的影响
    AStar（）没有发现明显bug 但是仍需完善，改进计算速度

--- AStar.py
+++ AStar.py
@@ -32,58 +32,58 @@ def findNeighbor(location,testBoard):
     """
     down
     """
-    if x < 6 and testBoard.board[x + 1, y * 3] == 1: #piece blow
-        if testBoard.board[x + 1, y * 3 + 2] == -1 : # block blow blow
-            if y > 0 and testBoard.board[x + 1,y * 3 - 2] == 1: #up left
+    if x < 6 and testBoard.board[x,y * 3 + 2] == 1:
+        if  testBoard.board[x + 1,y * 3] == 0:
+            neighbor.append((x + 1,y))
+        elif x < 5 and testBoard.board[x + 1,y * 3] == 1:
+            if testBoard.board[x + 1,y * 3 + 2] == 1:
+                neighbor.append((x + 2,y))
+            elif testBoard.board[x + 1,(y - 1) * 3 + 1] == 1:
                 neighbor.append((x + 1,y - 1))
-            if testBoard.board[x + 1,y * 3 + 1] == 1:
-                neighbor.append((x + 1,y + 1))  #up right
-        elif x < 5:
-            neighbor.append((x + 2,y))        
-    elif x < 6 and testBoard.board[x, y * 3 + 2] == 1:
-        neighbor.append((x + 1 ,y))
+            elif testBoard.board[x + 1,y * 3 + 1] == 1:
+                neighbor.append((x + 1,y + 1))
     """
     left
     """
-    if y > 0 and testBoard.board[x,(y - 1) * 3] == 1: #piece left
-        if y > 1 and testBoard.board[x,(y - 2) * 3 + 1] == -1: #block left left
-            if x > 0 and testBoard.board[x - 1,(y - 1) * 3 + 2] == 1: # left upon
+    if y > 0 and testBoard.board[x,(y - 1) * 3 + 1] == 1:
+        if  testBoard.board[x,(y - 1) * 3] == 0:
+            neighbor.append((x,y - 1))
+        elif y > 1 and testBoard.board[x,(y - 1) * 3] == 1:
+            if testBoard.board[x,(y - 2) * 3 + 1] == 1:
+                neighbor.append((x,y - 2))
+            elif testBoard.board[x - 1,(y - 1) * 3 + 2] == 1:
                 neighbor.append((x - 1,y - 1))
-            if testBoard.board[x,(y - 1) * 3 + 2] == 1: # left blow
+            elif testBoard.board[x,(y - 1) * 3 + 2] == 1:
                 neighbor.append((x + 1,y - 1))
-        elif y > 1:
-            neighbor.append((x, y - 2)) #left left
-    elif y > 0 and testBoard.board[x, (y - 1)* 3 + 1] == 1:
-        neighbor.append((x,y - 1)) #left
         
     """
     right
     """
-    if y < 6 and testBoard.board[x,(y + 1) * 3] == 1:  #piece right
-        if testBoard.board[x,(y + 1) * 3 + 1] == -1: #block right right
-            if x > 0 and testBoard.board[x - 1,(y + 1) * 3 + 2] == 1: #right upon
+    if y < 6 and testBoard.board[x,y * 3 + 1] == 1:
+        if  testBoard.board[x,(y + 1) * 3] == 0:
+            neighbor.append((x,y + 1))
+        elif y < 5 and testBoard.board[x,(y + 1) * 3] == 1:
+            if testBoard.board[x,(y + 1) * 3 + 1] == 1:
+                neighbor.append((x,y + 2))
+            elif testBoard.board[x - 1,(y + 1) * 3 + 2] == 1:
                 neighbor.append((x - 1,y + 1))
-            if testBoard.board[x,(y + 1) * 3 + 2] == 1: # right blow
+            elif testBoard.board[x,(y + 1) * 3 + 2] == 1:
                 neighbor.append((x + 1,y + 1))
-        elif y < 5:
-            neighbor.append((x,y + 2)) #right right 
-    elif y < 6 and testBoard.board[x,y * 3 + 1] == 1:
-        neighbor.append((x, y + 1)) # right
         
     """
     up
     """
-    if x > 0 and testBoard.board[x - 1,y * 3] == 1:  #piece upon
-        if x > 1 and testBoard.board[x - 2,y * 3 + 2] == -1: # block upon upon 
-            if y > 0 and testBoard.board[x - 1,y * 3 - 2] == 1:  #up left
+    if x > 0 and testBoard.board[x - 1,y * 3 + 2] == 1:
+        if  testBoard.board[x - 1,y * 3] == 0:
+            neighbor.append((x - 1,y))
+        elif x > 1 and testBoard.board[x - 1,y * 3] == 1:
+            if testBoard.board[x -2,y * 3 + 2] == 1:
+                neighbor.append((x - 2,y))
+            elif testBoard.board[x - 1,(y - 1) * 3 + 1] == 1:
                 neighbor.append((x - 1,y - 1))
-            if testBoard.board[x - 1,y * 3 + 1] == 1: #up right
+            elif testBoard.board[x - 1,y * 3 + 1] == 1:
                 neighbor.append((x - 1,y + 1))
-        elif x > 1:
-            neighbor.append((x - 2,y)) #up up
-    elif x > 0 and testBoard.board[x - 1,y * 3 + 2] == 1: 
-        neighbor.append((x - 1,y))# up
-     
+                
     return neighbor
           
 def AStar(owner,origin,testBoard):
@@ -98,6 +98,7 @@ def AStar(owner,origin,testBoard):
         sonPointInOpenlistFlag = 0
         neighbor = findNeighbor(searchPoint.location,testBoard)
         for i in neighbor:
+            sonPointInOpenlistFlag = 0
             sonPoint = Point(owner,i)
             sonPoint.fatherPoint = searchPoint.location
             sonPoint.g = searchPoint.g + 1
@@ -105,15 +106,18 @@ def AStar(owner,origin,testBoard):
             
             if sonPoint.location[0] == searchPoint.destination:
                 temp = 0
-            for i in testBoard.openList:
-                if sonPoint.location == i.location:
+#                 break
+            for searchRepeat in testBoard.openList:
+                if sonPoint.location == searchRepeat.location:
                     sonPointInOpenlistFlag = 1
-                    if sonPoint.g < i.g:
-                        i.fatherPoint = searchPoint.location
-                        i.g = searchPoint.g + 1
-                        i.f = i.g + i.h
+                    if sonPoint.g < searchRepeat.g:
+                        searchRepeat.fatherPoint = searchPoint.location
+                        searchRepeat.g = searchPoint.g + 1
+                        searchRepeat.f = searchRepeat.g + searchRepeat.h
+                    break
             if not sonPointInOpenlistFlag:
                 testBoard.openList.append(sonPoint)
+                
  
         minimalSearch = 0x3f3f3f3f
         for tempPoint in testBoard.openList:
@@ -124,6 +128,7 @@ def AStar(owner,origin,testBoard):
         testBoard.closeList.append(searchPoint) 
         tempPoint = testBoard.closeList[-1]  
         result = [tempPoint.location] 
+#         testBoard.movePiece(searchPoint.location,result)
     while not (tempPoint.location == origin):
         result.append(tempPoint.fatherPoint)
         for i in reversed(testBoard.closeList):
@@ -132,12 +137,18 @@ def AStar(owner,origin,testBoard):
                 testBoard.closeList.remove(i)
                 break
     print(result)
-    return len(result) #return distance
+    testBoard.openList = []
+    testBoard.closeList = []
+    return len(result) - 1 #return distance
 #         tempPoint = testBoard.closeList[testBoard.closeList.index(tempPoint.fatherPoint)]
 
 def evaluationFunc(testBoard):
+    testBoard.delPiece(testBoard.myPieceLocation)
     myStep = AStar(0, testBoard.myPieceLocation, testBoard)
-    print('my step is ',myStep)
+#     print('my step is ',myStep)
+    testBoard.setPiece(testBoard.myPieceLocation)
+    testBoard.delPiece(testBoard.enemysPieceLocation)
     enemyStep = AStar(1, testBoard.enemysPieceLocation, testBoard)
-    print('enemy"s step is ',enemyStep)
+#     print('enemy"s step is ',enemyStep)
+    testBoard.setPiece(testBoard.enemysPieceLocation)
     return enemyStep - myStep

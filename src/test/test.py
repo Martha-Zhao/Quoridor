ff9b925676f92097c3fac5116693214b9d1818b6
Martha-Zhao <zhao.martha@outlook.com>
2019-03-22 16:16:54 +0800

    内部函数调用测试
    集合所有内部函数测试

--- src/test/test.py
+++ src/test/test.py
@@ -1,41 +1,61 @@
-# from cheseBoard.preBoard import *
-from findRoute.ASearchFinal import *
-from findRoute.gVariables import CreateBoard
-origin = (0,3)
-end = (6,3)
-"""
-initialization piece and board
-"""
-myPiece = Piece(origin)# initialization my piece 
-myBoard = CreateBoard(origin,end) # initialization my board
+'''
+Created on 2019年3月22日
 
-myBoard.movePiece(origin, (1,5))
-myBoard.movePiece(end, (3,1))
-myPiece.location = (1,5)
-print(myBoard.board)
+@author: Martha Zhao
+@todo: all the test available
+'''
 
-"""
-place block
-"""
-myBoard.placeBlock(1, 2, 5) 
-myBoard.placeBlock(0, 1, 4)  
-myBoard.placeBlock(0, 3, 5)
+# from cheseBoard.preBoard import 
+from findRoute.AStar import findNeighbor,possibleBlockLocation,AStar
+from findRoute.gVariables import Board,Piece
+from findRoute.alphaBetaPruning import abS
 
-"""
-search path test
-"""
-# searchPath(myPiece,myBoard)
-# print(myBoard.C) #print result 
-
-"""
-possible piece location  test
-"""
-# result = possibleMoves(myPiece, myBoard)
-# print(result)
-
-"""
-possible blocks location test
-"""
-result = possibleBlockLocation(myPiece, myBoard)
-print(result)
-
+if __name__ == '__main__':
+    origin = (0,3)
+    end = (6,3)
+    """
+    initialization piece and board
+    """
+    myPiece = Piece(0,origin)# initialization my piece 
+    enemysPiece = Piece(1,end)
+    myBoard = Board(origin,end) # initialization my board
+    
+#     myBoard.movePiece(origin, (1,5))
+#     myBoard.movePiece(end, (3,1))
+#     print("origin board is ")
+#     print(myBoard.board)
+    
+    """
+    place block
+    """
+#     myBoard.placeBlock(1, 2, 5) 
+#     myBoard.placeBlock(0, 1, 4)  
+#     myBoard.placeBlock(0, 3, 5)
+     
+    """
+    possible piece location test
+    """
+#     res = findNeighbor(myPiece.location,myBoard)
+#     print("find neighbor test result is ") #print result
+#     print(res) 
+    
+    """
+    possible blocks location test
+    """
+#     res = possibleBlockLocation(myPiece.location, myBoard)
+#     print("possible block location test result is ")
+#     print(res)
+    
+    """
+    A* search test
+    """
+#     res = AStar(0,myPiece.location,myBoard)
+#     print ("A* test result is ")
+#     print ("res")
+    
+    """
+    alpha beta pruning test
+    """
+    res = abS(2, myBoard)
+    print ("alpha beta pruning test result is")
+    print(res)
